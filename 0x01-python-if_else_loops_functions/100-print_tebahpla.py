188ba6b5dab3d58fa67b3610573f580d582dc659
Tihno <justintihno@gmail.com>
2023-03-11 04:05:09 -0800

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(25, -1, -1):
-    print("{0}{1}".format(chr(97 + i), chr(65 + i)), end="")
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i -32
+    print("{}".format(chr(i)), end="")
