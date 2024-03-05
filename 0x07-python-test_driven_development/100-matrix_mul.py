f6fd22645ab77664fea1c9f62a95308fed176097
Tihno <justintihno@gmail.com>
2024-03-05 18:27:49 +0300

    task

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -32,25 +32,26 @@ def matrix_mul(m_a, m_b):
 
     if m_a == [] or m_a == [[]] or m_b == [] or m_b == [[]]:
         raise ValueError("m_a can't be empty"
-                if (m_a == [] or m_a == [[]]) else "m_b can't be empty")
+                            if (m_a == [] or m_a == [[]])
+                            else "m_b can't be empty")
 
     if any(not isinstance(num, (int, float))
             for row in m_a for num in row) or \
        any(not isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError("m_a should contain only integers or floats"
-                if any(not isinstance(num, (int, float))
-                    for row in m_a for num in row)
-                else "m_b should contain only integers or floats")
+                        if any(not isinstance(num, (int, float))
+                                for row in m_a for num in row)
+                        else "m_b should contain only integers or floats")
 
     if any(len(row) != len(m_a[0])
             for row in m_a) or any(len(row) != len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_a must be of the same size"
-                if any(len(row) != len(m_a[0]) for row in m_a)
-                else "each row of m_b must be of the same size")
+                            if any(len(row) != len(m_a[0]) for row in m_a)
+                            else "each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     result = [[sum(a * b for a, b in zip(row_a, col_b))
-            for col_b in zip(*m_b)] for row_a in m_a]
+                 for col_b in zip(*m_b)] for row_a in m_a]
     return result
